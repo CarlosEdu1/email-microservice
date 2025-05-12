9c52de19281d0a7dfd1b6602aa9106af8e9698cd
CarlosEdu1 <carloseedu1@outlook.com>
2025-05-12 00:07:43 -0300

    submited code

---
+++ src/main/java/com/ms/email/dtos/EmailRecordDto.java
@@ -0,0 +1,11 @@
+package com.ms.email.dtos;
+
+import java.util.UUID;
+
+public record EmailRecordDto(
+
+        UUID uuid,
+        String emailTo,
+        String subject,
+        String text) {
+}
